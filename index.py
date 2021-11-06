e6d0cb5e9c65c1d67ff4de343a4733849a9531b3
Alejandro Guacaneme <alejandroguacanemelara@gmail.com>
2021-11-06 15:01:50 -0500

    Finalizacion del proyecto

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, json, render_template, request, redirect,url_for,flash,make_response,jsonify
+from flask import Flask, render_template, request,json, redirect,url_for,flash,make_response,jsonify
 from flask.helpers import flash
 from flask_mysqldb import MySQL
 from werkzeug.utils import escape
@@ -17,14 +17,12 @@ def inicio():
     conexion = db.connection.cursor()
     conexion.execute("SELECT * FROM notas")
     data = conexion.fetchall()
-    print(data)
 
     return render_template('index.html',data=data)
 
 
 @app.route('/agregarNota', methods=["POST"])
 def agregarNota():
-    
     try:
         objNota = request.get_json()
 
@@ -35,50 +33,87 @@ def agregarNota():
         conexion.execute("INSERT INTO notas (titulo,descripcion,estado) VALUES (%s,%s,%s);",[titulo, descripcion,estado])
         db.connection.commit()
 
-        respuesta = make_response(jsonify({"message": "JSON received"}),200)
 
-        return respuesta
+
+        conexion.execute("SELECT * FROM notas")
+        data = conexion.fetchall()
+
+        jsonObj = json.dumps(data)
+
+        respuesta = make_response(jsonify(jsonObj),200)
+        return render_template('listadoNotas.html',data=data)
 
     except:
         respuesta = make_response(jsonify({"message": "ERROR MYSQL"}),500)
-
         return respuesta
-        #print(request.form['objNota'])
-        # flash("Contacto AGREGRADO CORRECTAMENTE")
-
 
 @app.route('/cambiarEstado',methods=["POST"])
 def cambiarEstado():
-
     try:
         objEstado = request.get_json()
 
         idNota = objEstado['id']
         estado = objEstado['estado']
-
-        print(idNota)
-        print(estado)
         conexion = db.connection.cursor()
         
         conexion.execute("UPDATE notas SET estado = (%s) WHERE id = %s;",[estado, idNota])
         db.connection.commit()
-        respuesta = make_response(jsonify({"message": "ESTADO RECIBIDO"}),200)
+        conexion.execute("SELECT * FROM notas")
+        data = conexion.fetchall()
 
-        return respuesta
+        jsonObj = json.dumps(data)
+
+        respuesta = make_response(jsonify(jsonObj),200)
+        return render_template('listadoNotas.html',data=data)
 
     except:
         respuesta = make_response(jsonify({"message": "ERROR MYSQL"}),500)
 
         return respuesta
-    return "hola"
 
-@app.route('/editarNota')
+@app.route('/editarNota',methods=["POST"])
 def editarNota():
-    return 'EDITAR NOTA'
+    try:
+        objNota = request.get_json()
 
-@app.route('/eliminarNota')
+        idNota = objNota['id']
+        titulo = objNota['titulo']
+        descripcion = objNota['descripcion']
+
+        conexion = db.connection.cursor()
+        conexion.execute("UPDATE notas SET titulo = (%s),descripcion = (%s) WHERE id = %s;",[titulo, descripcion,idNota])
+        db.connection.commit()
+
+        conexion.execute("SELECT * FROM notas")
+        data = conexion.fetchall()
+
+        jsonObj = json.dumps(data)
+
+        respuesta = make_response(jsonify(jsonObj),200)
+        return render_template('listadoNotas.html',data=data)
+
+    except:
+        respuesta = make_response(jsonify({"message": "ERROR MYSQL"}),500)
+        return respuesta
+
+@app.route('/eliminarNota',methods=["GET"])
 def eliminarNota():
-    return 'ELIMINAR NOTA'
+    try:
+        idNota = request.args.get("id")
+        conexion = db.connection.cursor()
+        
+        conexion.execute("DELETE FROM notas WHERE id = %s LIMIT 1;",[idNota,])
+        db.connection.commit()
+        conexion.execute("SELECT * FROM notas")
+        data = conexion.fetchall()
+
+        jsonObj = json.dumps(data)
+
+        respuesta = make_response(jsonify(jsonObj),200)
+        return render_template('listadoNotas.html',data=data)
+    except:
+        respuesta = make_response(jsonify({"message": "ERROR MYSQL"}),500)
+        return respuesta
 
 if __name__ == '__main__':
     app.run(port = 5000,debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,113 @@
 document.addEventListener('DOMContentLoaded', function(){
-    crearProducto();
+    crearNota();
     cambiarEstado();
+    eliminarNota();
+    cambiarFormulario();
+    actualizarNota();
 });
 
-function crearProducto(){
+function cargarListado(datos,cambiarForm = 0){
+    const listado = document.querySelector('#listadoNotas');
+    while(listado.firstChild){
+        listado.removeChild(listado.firstChild);
+    }
+    listado.insertAdjacentHTML('afterbegin',datos);
+    cambiarEstado();
+    eliminarNota();
+    cambiarFormulario(cambiarForm);
+    actualizarNota();
+}
+
+function cambiarFormulario(cambiar = 0){
+    if(document.querySelectorAll('.actualizarNota')[0]){
+        const actualizarNotas = document.querySelectorAll('.actualizarNota');
+        const divActualizar = document.querySelector('#formularioActualizar');
+        const divCrear = document.querySelector('#formularioCrear');
+        actualizarNotas.forEach(nota =>{
+            nota.addEventListener('click', e=>{
+                idNota = nota.dataset.actualizar;
+                const tituloNota = document.querySelector(`.titulo${idNota}`).textContent;
+                const descripcionNota = document.querySelector(`.descripcion${idNota}`).textContent;
+
+                divActualizar.classList.remove('hidden');
+                divCrear.classList.add('hidden');
+
+                document.querySelector('#tituloActualizar').value = tituloNota;
+                document.querySelector('#descripcionActualizar').value = descripcionNota;;
+                document.querySelector('#idNota').value = idNota;
+            });
+        });
+
+        if(cambiar != 0){
+            divActualizar.classList.add('hidden');
+            divCrear.classList.remove('hidden');
+        }
+    }
+}
+
+function actualizarNota(){
+    if(document.querySelector('#botonActualizar')){
+        const boton = document.querySelector('#botonActualizar');
+        boton.addEventListener('click', e =>{
+            e.preventDefault();
+            const idNota = document.querySelector('#idNota').value;
+            const titulo = document.querySelector('#tituloActualizar').value;
+            const descripcion = document.querySelector('#descripcionActualizar').value;
+            if(titulo != "" && descripcion != ""){
+                objNota = {
+                    'id': idNota,
+                    'titulo': titulo,
+                    'descripcion': descripcion
+                }
+
+                url = "/editarNota";
+                fetch(url,{
+                    method: 'POST',
+                    body: JSON.stringify(objNota),
+                    headers: {
+                        'Content-Type': 'application/json'// AQUI indicamos el formato
+                    }
+                })
+                .then(respuesta =>{
+                    return respuesta.text();
+                })
+                .then(datos => {
+                    cargarListado(datos,1);
+                    mensajeFormulario("Nota Actualizada",1)
+                })
+            }else{
+                mensajeFormulario("Todos los campos son obligatorios",0);
+            }
+        });
+    }
+}
+
+function eliminarNota(){
+    if(document.querySelectorAll('.eliminarNota')[0]){
+        const iconosEliminar = document.querySelectorAll('.eliminarNota');
+
+        iconosEliminar.forEach(icono =>{
+            icono.addEventListener('click', e =>{
+                e.preventDefault();
+                idNota = icono.dataset.eliminar;
+                url = `/eliminarNota?id=${idNota}`;
+                fetch(url,{
+                    method: 'GET',
+                    headers: {},
+                })
+                .then(respuesta =>{
+                    return respuesta.text();
+                })
+                .then(datos =>{
+                    cargarListado(datos);
+                    mensajeLista("Nota Eliminada",1);
+                })
+            });
+        });
+    }
+}
+
+function crearNota(){
     if(document.querySelector('#botonNota')){
         const boton = document.querySelector('#botonNota');
         boton.addEventListener('click', e =>{
@@ -11,35 +115,85 @@ function crearProducto(){
             const titulo = document.querySelector('#titulo').value;
             const descripcion = document.querySelector('#descripcion').value;
 
-            objNota = {
-                'titulo':titulo,
-                'descripcion': descripcion
-            }
-
-            url = "/agregarNota";
-            fetch(url,{
-                method: 'POST',
-                body: JSON.stringify(objNota),
-                headers: {
-                    'Content-Type': 'application/json'// AQUI indicamos el formato
+            if(titulo != "" && descripcion != ""){
+                objNota = {
+                    'titulo':titulo,
+                    'descripcion': descripcion
                 }
-            })
-            .then(respuesta =>{
-                return respuesta.json();
-            })
-            .then(datos => {
-                console.log(datos);
-            })
+    
+                url = "/agregarNota";
+                fetch(url,{
+                    method: 'POST',
+                    body: JSON.stringify(objNota),
+                    headers: {
+                        'Content-Type': 'application/json'// AQUI indicamos el formato
+                    }
+                })
+                .then(respuesta =>{
+                    return respuesta.text();
+                })
+                .then(datos => {
+                    cargarListado(datos);
+                    mensajeFormulario("Nota Agregada Correctamente",1);
+                })
+            }else{
+                mensajeFormulario("Todos los campos son obligatorios",0);
+            }
         });
     }
 }
 
+function mensajeFormulario(mensaje,tipo){
+    if(document.querySelector('#formulario')){
+        const alertaPrevia = document.querySelector('.alerta');
+        if(alertaPrevia) {
+            return;
+        }
+        const div = document.querySelector('#formulario');
+        const alerta = document.createElement('DIV');
+        alerta.textContent = mensaje;
+        alerta.classList.add('alerta');
+        if(tipo == 0){
+            alerta.classList.add('error');
+        }else if(tipo == 1){
+            alerta.classList.add('exito');
+        }
+        div.insertAdjacentElement('beforeend',alerta);
+
+        setTimeout( () => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
+function mensajeLista(mensaje,tipo){
+    if(document.querySelector('#listadoNotas')){
+        const alertaPrevia = document.querySelector('.alerta');
+        if(alertaPrevia) {
+            return;
+        }
+        const div =document.querySelector('#listadoNotas');
+        const alerta = document.createElement('DIV');
+        alerta.textContent = mensaje;
+        alerta.classList.add('alerta');
+        if(tipo == 0){
+            alerta.classList.add('error');
+        }else if(tipo == 1){
+            alerta.classList.add('exito');
+        }
+        div.insertAdjacentElement('afterbegin',alerta);
+
+        setTimeout( () => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
 function cambiarEstado(){
-    if(document.querySelectorAll('#estado')[0]){
-        const estados = document.querySelectorAll('#estado');
+    if(document.querySelectorAll('.estado')[0]){
+        const estados = document.querySelectorAll('.estado');
         estados.forEach(estado => {
             estado.addEventListener('change', e=>{
-                
                 valorEstado = estado.value;
                 idNota =e.target.dataset.idnota;
                 objEstado = {
@@ -59,7 +213,8 @@ function cambiarEstado(){
                         return respuesta.text()
                     })
                     .then(datos =>{
-                        console.log(datos);
+                        cargarListado(datos);
+                        mensajeLista("Estado Cambiado",1);
                     })
             });
         });
